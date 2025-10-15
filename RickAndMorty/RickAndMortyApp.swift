59571da8f5428528343ccfabd80c4b1006283884
Julio Cesar Reyes <jcreyes@tribalworldwide.gt>
2025-10-15 14:25:09 -0600

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Julio César Reyes on 15/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
